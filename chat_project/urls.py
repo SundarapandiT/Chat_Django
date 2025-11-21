0ab0072dcc4fe07635d4f31f7bc1c1253ff79ce9
sundarapandi-sfl <sundarapandi@sflworldwide.com>
2025-11-21 20:37:19 +0530

    always serve static/media

--- chat_project/urls.py
+++ chat_project/urls.py
@@ -1,7 +1,3 @@
-"""
-URL configuration for chat_project project.
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -16,17 +12,16 @@ urlpatterns = [
     path('', chat_home, name='chat-home'),
 
     path('admin/', admin.site.urls),
-    
+
     # API endpoints
     path('api/auth/', include('users.urls')),
     path('api/chat/', include('chat.urls')),
-    
+
     # JWT tokens
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Serve static + media ALWAYS inside container (Railway)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
